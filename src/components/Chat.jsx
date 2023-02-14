639e927d99d134cf886880fbb22963b5c2e1eb40
DezBooM <dezboom3@gmail.com>
2023-02-14 11:05:42 +0100

    Design bug

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -6,7 +6,7 @@ function Chat() {
   const { data } = useChatContext()
 
   return (
-    <div className="h-screen sm:h-auto flex-[3]">
+    <div className="h-screen flex-[3]">
       <div className="bg-green-900 py-4 h-16">
         <h1 className="text-center text-2xl">
           {data.user?.displayName
